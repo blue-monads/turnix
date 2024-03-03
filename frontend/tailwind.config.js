52d070d5ab6055dc4dcdc99f4d2c76d00391e012
bornjre <bornjre@gmail.com>
2024-03-03 17:40:15 +0545

    add tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,14 @@
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [skeleton({
+		themes: { preset: [ "skeleton" ] }
+	})],
 }
 
