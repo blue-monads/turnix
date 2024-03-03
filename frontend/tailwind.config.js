c0b42b605057ab30c07220e079b08acd545bf7ae
bornjre <bornjre@gmail.com>
2024-03-03 17:58:28 +0545

    nit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,18 @@
 import { skeleton } from '@skeletonlabs/tw-plugin';
+import { join } from 'path';
 
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+
   theme: {
     extend: {},
   },
