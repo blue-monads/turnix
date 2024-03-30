7432bf10c971f5afb13f5f1e3d6cbd8ac6f25a0f
bornjre <bornjre@gmail.com>
2024-03-31 02:09:06 +0545

    fixes

--- frontend/src/routes/portal/ptypes/onloop/queue/preview/+page.svelte
+++ frontend/src/routes/portal/ptypes/onloop/queue/preview/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import AutoForm from "../../../../../../lib/compo/autoform/auto_form.svelte";
+    import { Loader, AutoForm } from "$lib/compo";
+
     import { params } from "$lib/params";
     import type { API } from "$lib/api";
     import { getContext } from "svelte";
@@ -7,47 +8,55 @@
     let pid = $params["pid"];
     let mid = $params["mid"];
     let message = "";
-
+    let data = {};
+    let loading = true;
 
     const api = getContext("__api__") as API;
 
     const load = async () => {
-        
+        const resp = await api.getQueueMessage(pid, mid);
+        if (resp.status !== 200) {
+            return;
+        }
+        data = resp.data;
+        loading = false
+    };
 
-    }
-
-
-
+    load();
 </script>
 
+{#if loading}
+    <Loader />
+{:else}
+    <AutoForm
+        {data}
+        {message}
+        schema={{
+            fields: [
+                {
+                    ftype: "KEY_VALUE_TEXT",
+                    key_name: "contents",
+                    name: "Contents",
+                    disabled: true,
+                },
+                {
+                    ftype: "SELECT",
+                    key_name: "status",
+                    name: "Status",
+                    options: ["submited", "processed", "denied"],
+                    disabled: true,
+                },
 
-<AutoForm
-    data={{}}
-    {message}
-    schema={{
-        fields: [
-            {
-                ftype: "KEY_VALUE_TEXT",
-                key_name: "contents",
-                name: "Contents",
-            },
-            {
-                ftype: "SELECT",
-                key_name: "status",
-                name: "Status",
-                options: ["submited", "processed", "denied"],
-            },
-            {
-                ftype: "KEY_VALUE_TEXT",
-                key_name: "extrameta",
-                name: "Extra Meta",
-            },
-        ],
-        name: "Add Queue message",
-        required_fields: ["name"],
-    }}
-    onSave={async (newdata) => {
-
-
-    }}
-/>
+                {
+                    ftype: "KEY_VALUE_TEXT",
+                    key_name: "extrameta",
+                    name: "Extra Meta",
+                    disabled: true
+                },
+            ],
+            name: "Preview Queue message",
+            required_fields: [],
+        }}
+        onSave={async (newdata) => {}}
+    />
+{/if}
