8ce7021c22bc8da1516235c8842b65eb63d9288d
bornjre <bornjre@gmail.com>
2024-03-31 01:55:42 +0545

    expose component so its easier to import

---
+++ frontend/src/compo/index.ts
@@ -0,0 +1,7 @@
+import AutoForm from "./autoform/auto_form.svelte";
+import AutoTable from "./autotable/autotable.svelte";
+import PageLayout from "./pagelayout/pagelayout.svelte"
+import Loader from "./loader/loader.svelte";
+
+
+export { AutoForm, AutoTable, PageLayout, Loader }
