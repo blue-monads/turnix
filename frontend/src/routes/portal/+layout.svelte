d20ca908da974df46d142cb5ce1c9392c4d9442c
bornjre <bornjre@gmail.com>
2024-03-31 22:01:58 +0545

    mobile view nav

--- frontend/src/routes/portal/+layout.svelte
+++ frontend/src/routes/portal/+layout.svelte
@@ -13,6 +13,7 @@
 	import ContextThis from "./contextThis.svelte";
 
 	let currentTile = "";
+	let showMobileModal = false;
 
 	let sibarItems: object[] = [];
 
@@ -25,14 +26,73 @@
 		api = new API();
 	};
 
+	const toggle = () => {
+		showMobileModal = !showMobileModal;
+	};
+
 	load();
 </script>
 
+{#if showMobileModal}
+	<div class="h-screen w-screen bg-gray-600 bg-opacity-90 fixed z-50">
+		<div
+			class="h-full w-full absolute transform translate-x-0 transition ease-in-out duration-700 p-5"
+		>
+			<div class="h-full bg-white rounded p-5">
+				<div class="absolute right-4">
+					<button
+						on:click={toggle}
+						aria-label="close menu modal"
+						class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 cursor-pointer rounded border mr-2"
+					>
+						<Icon name="x-mark" class="h-4 w-4" />
+					</button>
+				</div>
+
+				<ul class="space-y-4 pt-10">
+					{#each sibarItems as item, index}
+						<li class="border rounded">
+							<a
+								on:click={toggle}
+								class="h-12 px-4 flex justify-center items-center w-full text-gray-700 focus:text-orange-500 uppercase gap-2"
+								href={item["link"]}
+							>
+								<Icon
+									name={item["icon"] || "link"}
+									class="w-6 h-6"
+								/>
+								{item["name"] || ""}
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</div>
+	</div>
+{/if}
+
 <AppShell>
-	<div slot="sidebarLeft" class="h-screen">
+	<div slot="header" class="flex flex-row md:hidden justify-between bg-surface-100-800-token">
+		<div class="flex p-1">
+			<button on:click={toggle}>
+				<Icon name="bars-3" class="w-6 h-6" />
+			</button>
+		</div>
+
+		<div class="flex gap-4 p-1">
+			<a
+				class="flex flex-col justify-center items-center rounded-full bg-secondary-100 hover:bg-secondary-200 p-2"
+				href={"/z/pages/portal/self"}
+			>
+				<Icon name="user" class="w-6 h-6" />
+			</a>
+		</div>
+	</div>
+
+	<div slot="sidebarLeft" class="h-screen hidden md:block">
 		<AppRail width="w-14">
 			<div slot="lead" class="mb-4">
-				<AppRailAnchor href="/">
+				<AppRailAnchor href="/z/pages/portal/projects">
 					<div class="flex justify-center items-center p-2">
 						<img src={Logo} alt="logo" />
 					</div>
@@ -54,10 +114,6 @@
 								name={item["icon"] || "link"}
 								class="w-6 h-6"
 							/>
-
-							<span class="text-xs">
-								{item["name"]}
-							</span>
 						</a>
 					</svelte:fragment>
 				</AppRailTile>
