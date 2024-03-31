6f5e88e0ac6793e31bb5a29d2273c48a0b208bfd
bornjre <bornjre@gmail.com>
2024-03-31 17:33:51 +0545

    add  ctx instead of getdata

--- backend/xtypes/xfiles/xfiles.go
+++ backend/xtypes/xfiles/xfiles.go
@@ -1,9 +1,21 @@
 package xfiles
 
+import "github.com/gin-gonic/gin"
+
+type EventType = uint8
+
+const (
+	EventTypeList EventType = iota
+	EventTypeAddFile
+	EventTypeAddFolder
+)
+
 type Event struct {
-	UserId    int64
-	ProjectId int64
-	Path      string
-	Action    string
-	GetData   func() ([]byte, error)
+	UserId      int64
+	ProjectId   int64
+	Type        EventType
+	Path        string
+	Action      string
+	HttpContext *gin.Context
+	Consumed    bool
 }
