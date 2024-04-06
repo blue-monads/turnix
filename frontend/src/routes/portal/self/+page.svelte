67d9319f91de1b51e2374d9facca587979d52405
bornjre <bornjre@gmail.com>
2024-04-07 04:44:26 +0545

    more placeholder stuff

--- frontend/src/routes/portal/self/+page.svelte
+++ frontend/src/routes/portal/self/+page.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
+    import { AppBar } from "@skeletonlabs/skeleton";
+
 </script>
 
-<div class="p-2">
+
+<AppBar>
+	<svelte:fragment slot="lead">
+        <h4 class="h4">Profile</h4>
+    </svelte:fragment>
+
+	<svelte:fragment slot="trail">
+        <a href="/z/pages/portal/self/users" class="btn variant-filled">Users</a>
+        <a href="/z/pages/portal/self/devices" class="btn variant-filled">Devices</a>
+    </svelte:fragment>
+</AppBar>
+
+
+<div class="p-2">    
     <div class="card p-2">
         <header class="flex justify-center">
             <div class="flex flex-col gap-2">
-                <h2 class="h2">Profile</h2>
-
                 <div class="w-16 h-16 rounded-full bg-gray-500"></div>
             </div>
         </header>
@@ -44,5 +57,22 @@
                 />
             </label>
         </section>
+
+        <footer class="flex justify-end p-2 gap-2">
+            <a href="/z/pages/portal/self/reset-password" class="btn variant-ghost-secondary" >
+                Change Password
+            </a>
+            
+            <a href="/z/pages/portal/self/reset-email" class="btn variant-ghost-tertiary" >
+                Reset email
+            </a>
+
+            <button class="btn variant-filled" disabled>
+                Update
+            </button>
+
+
+        </footer>
+
     </div>
 </div>
