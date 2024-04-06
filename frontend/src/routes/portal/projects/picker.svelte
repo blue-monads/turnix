07afa0dc4817542afa1f828fae62866568759a37
bornjre <bornjre@gmail.com>
2024-04-07 04:04:34 +0545

    stash

--- frontend/src/routes/portal/projects/picker.svelte
+++ frontend/src/routes/portal/projects/picker.svelte
@@ -1,3 +1,13 @@
-
-
-<div>Picker</div>
+<div class="card p-2 justify-center">
+    <div class="logo-cloud grid-cols-1 lg:!grid-cols-3 gap-1">
+        <a class="logo-item" href="/">
+            <span>Skeleton</span>
+        </a>
+        <a class="logo-item" href="/">
+            <span>Skeleton</span>
+        </a>
+        <a class="logo-item" href="/">
+            <span>Skeleton</span>
+        </a>
+    </div>
+</div>
--- frontend/src/routes/portal/self/+page.svelte
+++ frontend/src/routes/portal/self/+page.svelte
@@ -1,1 +1,20 @@
-<div> SELF PAGE</div>
+<script lang="ts">
+</script>
+
+
+<div class="p-2 ">
+    <div class="card p-2">
+        <header class="">
+            <h2 class="h2">User Information</h2>
+        </header>
+    
+        <section class="">
+            <label class="label">
+                <span>Input</span>
+                <input class="input" type="text" placeholder="Input" />
+            </label>
+        </section>
+    </div>
+    
+</div>
+
