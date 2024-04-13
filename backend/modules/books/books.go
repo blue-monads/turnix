4008e3bde3757430db04939b204577b616471c52
bornjre <bornjre@gmail.com>
2024-04-14 03:28:44 +0545

    nit

--- backend/modules/books/books.go
+++ backend/modules/books/books.go
@@ -1,13 +1,18 @@
 package books
 
 import (
+	"github.com/bornjre/trunis/backend/registry"
 	"github.com/bornjre/trunis/backend/xtypes/xproject"
 )
 
-var Defs = xproject.Defination{
+var defs = &xproject.Defination{
 	Name:                "Books",
 	Slug:                "books",
 	Info:                "Accounting made simpler",
 	Icon:                "book-open",
 	NewFormSchemaFields: []xproject.PTypeField{},
+}
+
+func init() {
+	registry.Register(defs)
 }
--- backend/modules/tracker/tracker.go
+++ backend/modules/tracker/tracker.go
@@ -1,13 +1,18 @@
 package tracker
 
-import "github.com/bornjre/trunis/backend/xtypes/xproject"
-
-var (
-	Defs = xproject.Defination{
-		Name:                "Trackers",
-		Slug:                "tracker",
-		Info:                "Track everything",
-		Icon:                "map-pin",
-		NewFormSchemaFields: []xproject.PTypeField{},
-	}
+import (
+	"github.com/bornjre/trunis/backend/registry"
+	"github.com/bornjre/trunis/backend/xtypes/xproject"
 )
+
+var defs = &xproject.Defination{
+	Name:                "Trackers",
+	Slug:                "tracker",
+	Info:                "Track everything",
+	Icon:                "map-pin",
+	NewFormSchemaFields: []xproject.PTypeField{},
+}
+
+func init() {
+	registry.Register(defs)
+}
