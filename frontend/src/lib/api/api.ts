9380919287ddc14520cbec70738cba34d7f23d52
bornjre <bornjre@gmail.com>
2024-04-14 03:59:07 +0545

    nit

--- frontend/src/lib/api/api.ts
+++ frontend/src/lib/api/api.ts
@@ -67,59 +67,6 @@ export class API {
         return this.client.delete(`/project/${pid}/user`)
     }
 
-    // templates
-
-    listTemplates = (pid: string) => {
-        return this.client.get(`/pt/laction/template/${pid}`)
-    }
-
-    addTemplate = (pid: string, data: object) => {
-        return this.client.post(`/pt/laction/template/${pid}`, data)
-    }
-
-    updateTemplate = (pid: string, tid: string, data: object) => {
-        return this.client.post(`/pt/laction/template/${pid}/${tid}`, data)
-    }
-
-    getTemplate = (pid: string, tid: string) => {
-        return this.client.get(`/pt/laction/template/${pid}/${tid}`)
-    }
-
-    removeTemplate = (pid: string, tid: string) => {
-        return this.client.delete(`/pt/laction/template/${pid}/${tid}`)
-    }
-
-    // queue
-
-    pushQueueMessage = (pid: string, tid: string, data: object) => {
-        return this.client.post(`/pt/laction/template/${pid}/${tid}/push`, data)
-    }
-
-    listQueueMessages = (pid: string) => {
-        return this.client.get(`/pt/laction/queue/${pid}`)
-    }
-
-
-    addQueueMessage = (pid: string, data: object) => {
-        return this.client.post(`/pt/laction/queue/${pid}`, data)
-    }
-
-    updateQueueMessage = (pid: string, qid: string, data: object) => {
-        return this.client.post(`/pt/laction/queue/${pid}/${qid}`, data)
-    }
-
-    getQueueMessage = (pid: string, mid: string) => {
-        return this.client.get(`/pt/laction/queue/${pid}/${mid}`)
-    }
-
-    removeUpdateQueueMessage = (pid: string, qid: string) => {
-        return this.client.delete(`/pt/laction/queue/${pid}/${qid}`)
-    }
-
-    queryQueueMessage = (pid: string) => {
-        return this.client.post(`/pt/laction/queue/${pid}`)
-    }
-
 }
 
 
