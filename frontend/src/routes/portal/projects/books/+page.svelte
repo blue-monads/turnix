5e657aaaf038f372e24b725a04352b609cf87160
bornjre <bornjre@gmail.com>
2024-04-20 08:24:09 +0545

    placeholder

--- frontend/src/routes/portal/projects/books/+page.svelte
+++ frontend/src/routes/portal/projects/books/+page.svelte
@@ -1,6 +1,16 @@
 <script>
+  import { goto } from "$app/navigation";
+  import { FloatyButton } from "$lib/compo";
   import ListProject from "../../listProject.svelte";
+  import { AppBar, getModalStore } from "@skeletonlabs/skeleton";
+
+  const store = getModalStore();
 </script>
 
+<ListProject ptype={"books"} />
 
-<ListProject ptype={"book"} />
+<FloatyButton
+  handler={() => {
+    goto("/z/pages/portal/projects/books/new");
+  }}
+/>
---
+++ frontend/src/routes/portal/projects/books/new/+page.svelte
@@ -0,0 +1,2 @@
+
+<div>New</div>
