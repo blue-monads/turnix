fe95296bf776b4f1c46bda0861f11b29a11e80f6
bornjre <bornjre@gmail.com>
2024-04-21 06:41:02 +0545

    nit

--- frontend/src/routes/portal/projects/books/+page.svelte
+++ frontend/src/routes/portal/projects/books/+page.svelte
@@ -1,15 +1,5 @@
 <script lang="ts">
   import ListProject from "../../listProject.svelte";
-  import { params } from "$lib/params";
-  import ListAccount from "./account/+page.svelte";
-
-  let pid = $params["pid"];
-
-
 </script>
 
-{#if pid}
-  <ListAccount />
-{:else}
-  <ListProject ptype="books" />
-{/if}
+<ListProject ptype="books" />
---
+++ frontend/src/routes/portal/projects/books/[pid]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  const pid = $page.params["pid"];
+  console.log("@pid", pid);
+</script>
+
+<div>Books {pid}</div>
