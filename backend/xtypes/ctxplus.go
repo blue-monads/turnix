77cc5a3dba52fa594e9c6a0f6db0725dcd2eb1d6
bornjre <bornjre@gmail.com>
2024-05-26 02:51:47 +0545

    fix bug forgot pass name

--- backend/xtypes/ctxplus.go
+++ backend/xtypes/ctxplus.go
@@ -23,7 +23,7 @@ func (c *ContextPlus) ProjectId() int64 {
 }
 
 func (c *ContextPlus) ParamInt64(name string) int64 {
-	id, err := strconv.ParseInt(c.Http.Param("pid"), 10, 64)
+	id, err := strconv.ParseInt(c.Http.Param(name), 10, 64)
 	if err != nil {
 		panic(err)
 	}
