1dbfa272b27ab3855c862d59b8667caf8a926b47
bornjre <bornjre@gmail.com>
2024-05-26 11:26:56 +0545

    reports placeholder pages

--- frontend/src/routes/portal/projects/books/[pid]/report/+page.svelte
+++ frontend/src/routes/portal/projects/books/[pid]/report/+page.svelte
@@ -1,1 +1,70 @@
-<div>Reports</div>
+<script lang="ts">
+    import SvgIcon from "$lib/compo/icons/SvgIcon.svelte";
+    import { NewBookAPI } from "$lib/projects/books";
+    import { getContext } from "svelte";
+    import { AppBar, getModalStore } from "@skeletonlabs/skeleton";
+    import type { RootAPI } from "$lib/api";
+    import { page } from "$app/stores";
+  
+    const pid = $page.params["pid"];
+  
+    const store = getModalStore();
+    const api = NewBookAPI(getContext("__api__") as RootAPI);
+  
+    let accounts: any[] = [];
+  
+    const load = async () => {
+      const resp = await api.listAccount(pid);
+      if (resp.status !== 200) {
+        return;
+      }
+  
+      accounts = resp.data;
+    };
+  
+    load();
+  </script>
+  
+  <AppBar>
+    <svelte:fragment slot="lead">
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <a class="anchor" href="/z/pages/portal/projects/books">Books</a>
+        </li>
+        <li class="crumb-separator" aria-hidden>&rsaquo;</li>
+        <li>Reports</li>
+      </ol>
+    </svelte:fragment>
+  
+    <svelte:fragment slot="trail">
+      <a
+        href={`/z/pages/portal/projects/books/${pid}/report/live`}
+        class="btn variant-filled btn-sm"
+      >
+        <SvgIcon className="h-4 w-4" name="bolt" />
+  
+        Live Report
+      </a>
+  
+      <a
+        href={`/z/pages/portal/projects/books/${pid}/report/template`}
+        class="btn variant-filled-secondary btn-sm"
+      >
+        <SvgIcon className="h-4 w-4" name="code-bracket-square" />
+
+        Templates
+        
+      </a>
+  
+      <a
+        href={`/z/pages/portal/projects/books/${pid}`}
+        class="btn variant-glass-secondary btn-sm"
+      >
+        <SvgIcon className="h-4 w-4" name="users" />
+  
+        Accounts
+      </a>
+  
+
+    </svelte:fragment>
+  </AppBar>
---
+++ frontend/src/routes/portal/projects/books/[pid]/report/live/+page.svelte
@@ -0,0 +1,2 @@
+
+<div>Live reports</div>
