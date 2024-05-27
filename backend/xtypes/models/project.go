f950fbfc02db4fe25d9b8db64fb3583ee57d3bac
bornjre <bornjre@gmail.com>
2024-05-27 16:44:04 +0545

    nit

--- backend/xtypes/models/project.go
+++ backend/xtypes/models/project.go
@@ -21,7 +21,7 @@ type ProjectUser struct {
 }
 
 type ProjectHook struct {
-	ID          int64  `json:"id" db:"id"`
+	ID          int64  `json:"id" db:"id,omitempty"`
 	Event       string `json:"event" db:"event"`
 	OrderID     int64  `json:"order_id" db:"order_id"`
 	RunasUserID int64  `json:"runas_user_id" db:"runas_user_id"`
@@ -29,7 +29,7 @@ type ProjectHook struct {
 	HookCode    string `json:"hook_code" db:"hook_code"`
 	Envs        string `json:"envs" db:"envs"`
 	ProjectID   int64  `json:"project_id" db:"project_id"`
-	Extrameta   string `json:"extrameta" db:"extrameta"`
+	Extrameta   string `json:"extrameta" db:"extrameta,omitempty"`
 }
 
 type ProjectTypes struct {
