cdb29f2c74a89e39b662aaa2d44ae23c1c16601e
bornjre <bornjre@gmail.com>
2024-08-10 13:55:20 +0545

    nit

--- frontend/src/routes/portal/projects/books/[pid]/actions/import/+page.svelte
+++ frontend/src/routes/portal/projects/books/[pid]/actions/import/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    let name = "";
     let file: FileList | null = null;
 </script>
 
@@ -13,23 +12,13 @@
 
         <section class="flex flex-col gap-4">
             <label class="label">
-                <span>Name</span>
-                <input
-                    bind:value={name}
-                    class="input p-1"
-                    type="text"
-                    placeholder="TestProject1"
-                />
-            </label>
-
-            <label class="label">
                 <span>File</span>
                 <input type="file" bind:files={file} required />
             </label>
         </section>
 
         <header class="flex justify-end p-2">
-            <button disabled={!name || !file} class="btn btn-sm variant-filled" on:click={() => {
+            <button disabled={ !file} class="btn btn-sm variant-filled" on:click={() => {
 
             }}
                 >Submit</button
