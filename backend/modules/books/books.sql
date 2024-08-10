79018e3cf3b45fabf65abf6d8bcae3126c9754fb
bornjre <bornjre@gmail.com>
2024-08-10 21:00:09 +0545

    invoice and stock management change

--- backend/modules/books/books.sql
+++ backend/modules/books/books.sql
@@ -1,3 +1,6 @@
+
+-- ACCOUNTING 
+
 create table Accounts__project__(
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL DEFAULT '',
@@ -21,6 +24,7 @@ create table Transactions__project__(
     txn_type TEXT NOT NULL DEFAULT 'normal',
     linked_sales_id INTEGER NOT NULL DEFAULT 0,
     linked_invoice_id INTEGER NOT NULL DEFAULT 0,
+    linked_bill_id INTEGER NOT NULL DEFAULT 0,
     reference_id TEXT NOT NULL DEFAULT '',
     attachments TEXT NOT NULL DEFAULT '',
     created_by INTEGER NULL,
@@ -60,6 +64,131 @@ create table Reports__project__(
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+-- INVOICING
+
+create table Invoices__project__(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL DEFAULT '',
+    notes TEXT NOT NULL DEFAULT '',
+    attachments TEXT NOT NULL DEFAULT '',
+    tax_id INTEGER NOT NULL DEFAULT 0,
+    sub_total INTEGER NOT NULL DEFAULT 0,
+    client_id INTEGER NOT NULL DEFAULT 0,
+    vendor_id INTEGER NOT NULL DEFAULT 0,
+    total INTEGER NOT NULL DEFAULT 0,
+    created_by INTEGER NULL,
+    updated_by INTEGER NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+
+create table InvoiceLines__project__(
+    id INTEGER PRIMARY KEY,
+    info TEXT NOT NULL DEFAULT '',
+    qty INTEGER NOT NULL DEFAULT 0,    
+    invoice_id INTEGER NOT NULL,
+    amount INTEGER NOT NULL DEFAULT 0,
+    created_by INTEGER NULL,
+    updated_by INTEGER NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+create table Bills__project__(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL DEFAULT '',
+    notes TEXT NOT NULL DEFAULT '',
+    attachments TEXT NOT NULL DEFAULT '',
+    sub_total INTEGER NOT NULL DEFAULT 0,
+    total INTEGER NOT NULL DEFAULT 0,
+    client_id INTEGER NOT NULL DEFAULT 0,
+    vendor_id INTEGER NOT NULL DEFAULT 0,
+    created_by INTEGER NULL,
+    updated_by INTEGER NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+-- INVENTORY
+
+create table Catagories__project__(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL DEFAULT '',
+    info TEXT NOT NULL DEFAULT '',
+    parent_id INTEGER NOT NULL DEFAULT 0,
+    created_by INTEGER NULL,
+    updated_by INTEGER NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+
+create table Products__project__(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL DEFAULT '',
+    info TEXT NOT NULL DEFAULT '',
+    variant_id TEXT NOT NULL DEFAULT '',
+    price INTEGER NOT NULL DEFAULT 0,
+    parent_id INTEGER NOT NULL DEFAULT 0,
+    images TEXT NOT NULL DEFAULT '',
+    -- epoch INTEGER NOT NULL DEFAULT 0,
+    created_by INTEGER NULL,
+    updated_by INTEGER NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+
+create table ProductStockIn__project__(
+    id INTEGER PRIMARY KEY,
+    product_id INTEGER NOT NULL,
+    qty INTEGER NOT NULL DEFAULT 0,
+    supplier_id INTEGER NOT NULL DEFAULT 0,
+    bill_id INTEGER NOT NULL DEFAULT 0,
+    created_by INTEGER NULL,
+    updated_by INTEGER NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- SALES
+
+create table Sales__project__(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL DEFAULT '',
+    notes TEXT NOT NULL DEFAULT '',
+    attachments TEXT NOT NULL DEFAULT '',
+    tax_id INTEGER NOT NULL DEFAULT 0,
+    sub_total INTEGER NOT NULL DEFAULT 0,
+    total INTEGER NOT NULL DEFAULT 0,
+    txn_link_id INTEGER NOT NULL DEFAULT 0,
+    created_by INTEGER NULL,
+    updated_by INTEGER NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+
+create table SalesLines__project__(
+    id INTEGER PRIMARY KEY,
+    info TEXT NOT NULL DEFAULT '',
+    qty INTEGER NOT NULL DEFAULT 0,    
+    sale_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL DEFAULT 0,
+    amount INTEGER NOT NULL DEFAULT 0,
+    created_by INTEGER NULL,
+    updated_by INTEGER NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 
