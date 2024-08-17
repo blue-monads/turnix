b7c0ba96bedb038aba6c8ce37bb40df0a2f0d3b8
bornjre <bornjre@gmail.com>
2024-08-17 09:16:10 +0545

    implement sales functionality

--- backend/modules/books/dbops/sales.go
+++ backend/modules/books/dbops/sales.go
@@ -1,45 +1,29 @@
 package dbops
 
-/*
+import (
+	"context"
+	"fmt"
+	"time"
 
-// sales
+	"github.com/bornjre/turnix/backend/modules/books/models"
+	"github.com/upper/db/v4"
+)
+
+// fixme: add updated at everywhere
 
 type SalesData struct {
-	Title       string `json:"title"`
-	ClientID    int64  `json:"client_id"`
-	ClientName  string `json:"client_name"`
-	Notes       string `json:"notes"`
-	Attachments string `json:"attachments"`
-
-	TotalItemPrice          float64 `json:"total_item_price"`
-	TotalItemTaxAmount      float64 `json:"total_item_tax_amount"`
-	TotalItemDiscountAmount float64 `json:"total_item_discount_amount"`
-
-	SubTotal              float64         `json:"sub_total"`
-	OverallDiscountAmount float64         `json:"overall_discount_amount"`
-	OverallTaxAmount      float64         `json:"overall_tax_amount"`
-	Total                 float64         `json:"total"`
-	Lines                 []SalesLineData `json:"lines"`
-}
-
-type SalesLineData struct {
-	Info           string  `json:"info"`
-	Qty            float64 `json:"qty"`
-	SaleID         int64   `json:"sale_id"`
-	ProductID      int64   `json:"product_id"`
-	Price          float64 `json:"price"`
-	TaxAmount      float64 `json:"tax_amount"`
-	DiscountAmount float64 `json:"discount_amount"`
-	TotalAmount    float64 `json:"total_amount"`
+	Sales models.Sales       `json:"sales"`
+	Lines []models.SalesLine `json:"lines"`
 }
 
 type ProductLite struct {
-	ID         int64 `json:"id" db:"id,omitempty"`
-	StockCount int64 `json:"stock_count" db:"stock_count"`
-	Epoch      int64 `json:"epoch" db:"epoch"`
+	ID         int64  `json:"id" db:"id,omitempty"`
+	Name       string `json:"name" db:"name"`
+	StockCount int64  `json:"stock_count" db:"stock_count"`
+	Epoch      int64  `json:"epoch" db:"epoch"`
 }
 
-func (b *BookModule) dbOpsSalesAdd(pid, uid int64, data *SalesData) (int64, error) {
+func (b *DbOps) SalesAdd(pid, uid int64, data *SalesData) (int64, error) {
 	err := b.userHasScope(pid, uid, "write")
 	if err != nil {
 		return 0, err
@@ -47,55 +31,56 @@ func (b *BookModule) dbOpsSalesAdd(pid, uid int64, data *SalesData) (int64, erro
 
 	t := time.Now()
 
-	sd := Sales{
-		Title:       data.Title,
-		ClientID:    data.ClientID,
-		ClientName:  data.ClientName,
-		Notes:       data.Notes,
-		Attachments: data.Attachments,
-
-		TotalItemPrice:          data.TotalItemPrice,
-		TotalItemTaxAmount:      data.TotalItemTaxAmount,
-		TotalItemDiscountAmount: data.TotalItemDiscountAmount,
-
-		SubTotal:              data.SubTotal,
-		OverallDiscountAmount: data.OverallDiscountAmount,
-		OverallTaxAmount:      data.OverallTaxAmount,
-		Total:                 data.Total,
-
-		CreatedBy: uid,
-		UpdatedBy: uid,
-		CreatedAt: &t,
-		UpdatedAt: &t,
-		IsDeleted: false,
-	}
+	sd := data.Sales
+	sd.CreatedBy = uid
+	sd.UpdatedBy = uid
+	sd.CreatedAt = &t
+	sd.UpdatedAt = &t
+	sd.IsDeleted = false
 
 	salesId := int64(0)
 
-	productTable := b.productTable(pid)
-
-	// salelines product ids
-
 	productIds := make([]int64, 0, len(data.Lines))
+	requiredProductCount := make(map[int64]int64)
 
 	for _, line := range data.Lines {
 		productIds = append(productIds, line.ProductID)
+		oldQty := requiredProductCount[line.ProductID]
+		requiredProductCount[line.ProductID] = oldQty + line.Qty
 	}
 
 	products := make([]ProductLite, 0, len(data.Lines))
 
-	if err != nil {
-		return 0, err
-	}
-
-	if len(products) != len(productIds) {
-		return 0, errors.New("product not found")
-	}
-
 	b.db.GetSession().TxContext(context.Background(), func(tx db.Session) error {
 
-		salesTable := tx.Collection("z_%d_Sales")
-		salesLineTables := tx.Collection("z_%d_SalesLines")
+		productTable := tx.Collection(productTableName(pid))
+		salesTable := tx.Collection(salesTableName(pid))
+		salesLineTables := tx.Collection(salesLineTableName(pid))
+
+		err = productTable.Find(db.Cond{
+			"id IN":      productIds,
+			"is_deleted": false,
+		}).Select(
+			"id",
+			"stock_count",
+			"epoch",
+			"name",
+		).All(&products)
+		if err != nil {
+			return err
+		}
+
+		for _, product := range products {
+			requiredCount := requiredProductCount[product.ID]
+
+			if requiredCount == 0 {
+				return fmt.Errorf("product quantity should not be zero, %s", product.Name)
+			}
+
+			if requiredCount < product.StockCount {
+				return fmt.Errorf("product %s stock not enough, required: %d, available: %d", product.Name, requiredCount, product.StockCount)
+			}
+		}
 
 		r, err := salesTable.Insert(sd)
 		if err != nil {
@@ -106,63 +91,67 @@ func (b *BookModule) dbOpsSalesAdd(pid, uid int64, data *SalesData) (int64, erro
 
 		for _, line := range data.Lines {
 
-			saleLine := SalesLine{
-				Info:           line.Info,
-				Qty:            line.Qty,
-				SaleID:         salesId,
-				ProductID:      line.ProductID,
-				Price:          line.Price,
-				TaxAmount:      line.TaxAmount,
-				DiscountAmount: line.DiscountAmount,
-				TotalAmount:    line.TotalAmount,
-				CreatedBy:      uid,
-				UpdatedBy:      uid,
-				CreatedAt:      &t,
-				UpdatedAt:      &t,
-				IsDeleted:      false,
-			}
+			line.SaleID = salesId
+			line.CreatedBy = uid
+			line.UpdatedBy = uid
+			line.CreatedAt = &t
+			line.UpdatedAt = &t
+			line.IsDeleted = false
 
-			_, err := salesLineTables.Insert(saleLine)
+			_, err := salesLineTables.Insert(line)
 			if err != nil {
 				return err
 			}
 
 		}
 
+		for _, product := range products {
+
+			reqamt := requiredProductCount[product.ID]
+			pamt := product.StockCount - reqamt
+
+			err = productTable.Find(db.Cond{
+				"id": product.ID,
+			}).Update(db.Cond{
+				"stock_count": pamt,
+			})
+			if err != nil {
+				return err
+			}
+		}
+
 		return nil
 	}, nil)
 
-	table := b.salesTable(pid)
-
-	r, err := table.Insert(data)
-	if err != nil {
-		return 0, err
-	}
-
-	salesId = r.ID().(int64)
-
-	return r.ID().(int64), nil
+	return salesId, nil
 }
 
-func (b *BookModule) dbOpsSalesUpdate(pid, uid, id int64, data map[string]any) error {
-	err := b.userHasScope(pid, uid, "write")
-	if err != nil {
-		return err
-	}
-
-	return b.salesTable(pid).Find(db.Cond{"id": id}).Update(data)
+type SalesGetData struct {
+	SaleData models.Sales       `json:"sale_data"`
+	Lines    []models.SalesLine `json:"lines"`
 }
 
-func (b *BookModule) dbOpsSalesGet(pid, uid, id int64) (*Sales, error) {
+func (b *DbOps) SalesGet(pid, uid, id int64) (*SalesGetData, error) {
 	err := b.userHasScope(pid, uid, "read")
 	if err != nil {
 		return nil, err
 	}
 
-	data := &Sales{}
-	table := b.salesTable(pid)
+	data := &SalesGetData{
+		SaleData: models.Sales{},
+		Lines:    make([]models.SalesLine, 0),
+	}
 
-	err = table.Find(db.Cond{"id": id}).One(data)
+	salestable := b.salesTable(pid)
+
+	err = salestable.Find(db.Cond{"id": id}).One(&data.SaleData)
+	if err != nil {
+		return nil, err
+	}
+
+	saleLinetable := b.salesLineTable(pid)
+
+	err = saleLinetable.Find(db.Cond{"sale_id": id}).All(&data.Lines)
 	if err != nil {
 		return nil, err
 	}
@@ -170,13 +159,13 @@ func (b *BookModule) dbOpsSalesGet(pid, uid, id int64) (*Sales, error) {
 	return data, nil
 }
 
-func (b *BookModule) dbOpsSalesList(pid, uid int64) ([]Sales, error) {
+func (b *DbOps) SalesList(pid, uid int64) ([]models.Sales, error) {
 	err := b.userHasScope(pid, uid, "read")
 	if err != nil {
 		return nil, err
 	}
 
-	datas := make([]Sales, 0)
+	datas := make([]models.Sales, 0)
 	table := b.salesTable(pid)
 
 	err = table.Find().All(&datas)
@@ -187,17 +176,21 @@ func (b *BookModule) dbOpsSalesList(pid, uid int64) ([]Sales, error) {
 	return datas, nil
 }
 
-func (b *BookModule) salesTable(pid int64) db.Collection {
-	return b.db.Table(fmt.Sprintf("z_%d_Sales", pid))
+func (b *DbOps) salesTable(pid int64) db.Collection {
+	return b.db.Table(salesTableName(pid))
 }
 
-func (b *BookModule) salesLineTable(pid int64) db.Collection {
-	return b.db.Table(fmt.Sprintf("z_%d_SalesLines", pid))
+func (b *DbOps) salesLineTable(pid int64) db.Collection {
+	return b.db.Table(salesLineTableName(pid))
 }
 
+func salesTableName(pid int64) string {
+	return fmt.Sprintf("z_%d_Sales", pid)
+}
 
-
-*/
+func salesLineTableName(pid int64) string {
+	return fmt.Sprintf("z_%d_SalesLines", pid)
+}
 
 /*
 
