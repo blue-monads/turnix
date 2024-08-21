6d48425a747b603879859b92be3ef405649f1e25
bornjre <bornjre@gmail.com>
2024-08-21 22:39:29 +0545

    fixes

--- frontend/src/routes/portal/project/plugins/new/LiveEditor/+page.svelte
+++ frontend/src/routes/portal/project/plugins/new/LiveEditor/+page.svelte
@@ -1,2 +1,126 @@
+<script lang="ts">
+    import {
+        AppBar,
+        getModalStore,
+        Tab,
+        TabGroup,
+    } from "@skeletonlabs/skeleton";
+    import CodeMirror from "svelte-codemirror-editor";
+    import { html } from "@codemirror/lang-html";
+    import {
+        javascript,
+        localCompletionSource,
+        scopeCompletionSource,
+    } from "@codemirror/lang-javascript";
+    import { autocompletion } from "@codemirror/autocomplete";
+    import { NewBookAPI } from "$lib/projects/books";
+    import { getContext } from "svelte";
+    import type { RootAPI } from "$lib/api";
+    import SvgIcon from "$lib/compo/icons/SvgIcon.svelte";
+    import { sampleHtml } from "./sample";
+    import { params } from "$lib/params";
+    import Runner from "../../run/Runner.svelte";
 
-<div>Live editor</div>
+    let tabSet: number = 0;
+    let serverCode = "";
+    let clientCode = sampleHtml;
+    let savedClientCode = "";
+
+    const pid = $params["pid"];
+
+    const rootApi = getContext("__api__") as RootAPI;
+    const api = NewBookAPI(rootApi);
+
+    interface Message {
+        type: "api_call" | "ping";
+        name?: string;
+        data: any;
+        msgId: number;
+    }
+
+
+    const  completionObject = {}
+
+
+    const onFrameMessage = async (ev: MessageEvent) => {
+       
+    };
+</script>
+
+<AppBar>
+    <svelte:fragment slot="lead">
+        <ol class="breadcrumb">
+            <li class="crumb">
+                <a class="anchor" href="/z/pages/portal/project">Projects</a
+                >
+            </li>
+            <li class="crumb-separator" aria-hidden>&rsaquo;</li>
+            <li>
+                <a class="anchor" href={`/z/pages/portal/project/plugins?pid=${pid}`}>
+                    Plugins
+                </a>
+            </li>
+            <li class="crumb-separator" aria-hidden>&rsaquo;</li>
+            <li>Editor</li>
+        </ol>
+    </svelte:fragment>
+
+    <svelte:fragment slot="trail">
+        <button
+            class="btn variant-filled btn-sm"
+            on:click={() => {
+                savedClientCode = clientCode;
+
+            }}
+        >
+            <SvgIcon className="w-4 h-4" name="play" />
+            Run
+        </button>
+
+    </svelte:fragment>
+</AppBar>
+
+<div class=" flex flex-col md:flex-row w-full h-[94vh]">
+    <div class="flex-1 w-full md:w-1/2 border border-slate-50 h-1/2 md:h-full">
+        <TabGroup>
+            
+            <Tab padding="ml-6 p-2" bind:group={tabSet} name="SQL Query" value={0}>
+                <span>Server Code</span>
+            </Tab>
+            <Tab bind:group={tabSet} name="UI" value={1}>
+                <span>Client Code</span>
+            </Tab>
+
+            <svelte:fragment slot="panel">
+                <div class="max-h-[40vh] md:max-h-[90vh] overflow-auto"> 
+                    {#if tabSet === 0}
+                        <CodeMirror
+                            bind:value={serverCode}
+
+                            extensions={[
+                                autocompletion({
+                                    activateOnTyping: true,
+                                    override: [
+                                        localCompletionSource,
+                                        scopeCompletionSource(
+                                            completionObject,
+                                        ),
+                                    ],
+                                }),
+                            ]}
+
+                            lang={javascript()}
+                            
+                        />
+                    {:else if tabSet === 1}
+                        <CodeMirror bind:value={clientCode} lang={html()} />
+                    {/if}
+                </div>
+            </svelte:fragment>
+        </TabGroup>
+    </div>
+
+    <div class="flex-1 w-full md:w-1/2 border border-slate-50 h-1/2 md:h-full p-2">
+        <Runner client_code={savedClientCode} />        
+    </div>
+</div>
---
+++ frontend/src/routes/portal/project/plugins/new/LiveEditor/sample.ts
@@ -0,0 +1,103 @@
+
+
+
+export const sampleHtml = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="//unpkg.com/alpinejs" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script>
+        let pendingMessages = {}
+        let messageId = 0;
+        window.addEventListener("message", (ev) => {
+            console.log("message", ev);
+            if (ev.data !== "transfer_port") {
+                console.log("wrong message", ev);
+                return;
+            }
+            const port = ev.ports[0];
+            port.onmessage = handleMessage;
+            window["__parent_port__"] = port;
+        }, false);
+        const handleMessage = (ev) => {
+            const data = ev.data;
+            const messageId = data.msgId;
+            const resolve = pendingMessages[messageId];
+            delete pendingMessages[messageId];
+            resolve(data.data);
+            console.log("data", data);
+        }
+        const sendMessage = async (data) => {
+            console.log("sendMessage", data);
+            const msgId = messageId + 1;
+            messageId = msgId;
+            const p = new Promise((resolve) => {
+                pendingMessages[msgId] = resolve;
+            });
+            data["msgId"] = msgId;
+            const port = window["__parent_port__"]
+            port?.postMessage(data);
+            return p;
+        }
+    </script>
+</head>
+<body>
+    <div class="container">
+        <h1>Example Plugin UI</h1>          
+    </div>
+    <div class="container-xl mt-5" x-data="Loader()">
+        <div class="container">
+            <button id="start" class="btn btn-primary" type="button" x-on:click="load">
+                <span class="icon icon-thumbs-up"></span>
+                Start
+            </button>    
+        </div>
+    </div>
+
+    <script>
+    function Loader() {
+      return {
+          datas: [],
+          loading: true,
+          async load() {
+              this.loading = true;
+              try {
+
+                  const data = await sendMessage({
+                      type: "ping",
+                      name: "hello",
+                      data: [],
+                  });
+
+                  console.log("@data", data);
+
+                  this.data = data.data;
+              } catch (error) {
+                  console.error("Error fetching data:", error);
+              } finally {
+                  this.loading = false;
+              }
+          },
+          formatDate(dateString) {
+              return new Date(dateString).toLocaleDateString();
+          }
+      }
+  }
+
+
+    </script>
+</body>
+</html>
+
+
+
+
+
+
+
+`
