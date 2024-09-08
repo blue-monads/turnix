134ee6ec37eb283ab2f621acb7363c23b0e7d4fb
bornjre <bornjre@gmail.com>
2024-09-09 02:02:51 +0545

    nit

--- frontend/src/routes/portal/filestore/FilePicker.svelte
+++ frontend/src/routes/portal/filestore/FilePicker.svelte
@@ -1,19 +1,35 @@
 <script lang="ts">
-    import { RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
+    import { AppBar, RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
+    import FileListings from "../self/files/FileListings.svelte";
 
     let value = 0;
 </script>
 
 <div class="w-modal-wide bg-white p-4">
-    <div class="flex justify-between">
-        <h4 class="h4">File Picker</h4>
-        <RadioGroup>
-            <RadioItem bind:group={value} name="justify" value={0}
-                >Personal</RadioItem
-            >
-            <RadioItem bind:group={value} name="justify" value={1}
-                >Project</RadioItem
-            >
-        </RadioGroup>
+    <AppBar>
+        <svelte:fragment slot="lead">
+            <h4 class="h4">File Picker</h4>
+        </svelte:fragment>
+
+        <svelte:fragment slot="trail">
+            <RadioGroup>
+                <RadioItem bind:group={value} name="justify" value={0}
+                    >Personal
+                </RadioItem>
+                <RadioItem bind:group={value} name="justify" value={1}>
+                    Project
+                </RadioItem>
+            </RadioGroup>
+        </svelte:fragment>
+    </AppBar>
+
+    <div class="p-2">
+        <FileListings
+            baseUrl=""
+            path=""
+            files={[]}
+            loading={false}
+            selected=""
+        />
     </div>
 </div>
