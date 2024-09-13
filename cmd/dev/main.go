6d218bd19ed82f9f6413b7a59be4091f87c505d0
bornjre <bornjre@gmail.com>
2024-09-14 01:21:28 +0545

    move dev server main so root main can be used by root cli

---
+++ cmd/dev/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+
+	// modules
+
+	"github.com/bornjre/turnix/backend/distro"
+
+	_ "github.com/bornjre/turnix/backend/modules/books"
+
+	// _ "github.com/bornjre/turnix/backend/modules/tracker"
+	_ "github.com/bornjre/turnix/backend/modules/unloop"
+)
+
+func main() {
+
+	app, err := distro.NewApp()
+	if err != nil {
+		panic(err)
+	}
+
+	defer app.Stop()
+
+	mig, err := app.NeedsMigrate()
+	if err != nil {
+		panic(err)
+	}
+
+	if mig {
+		err = app.RunNormalSeed()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = app.Start()
+	if err != nil {
+		panic(err)
+	}
+
+}
--- justfile
+++ justfile
@@ -2,7 +2,7 @@
 start_dev_frontend:
 	cd frontend && npm run dev
 start_dev_backend:
-	FRONTEND_DEV_SERVER="http://localhost:5173" go run -v main.go
+	FRONTEND_DEV_SERVER="http://localhost:5173" go run -v cmd/dev/main.go
 run_tests:
 	cd tmp && rm -rf data.db && go run ../tests/*.go
 
