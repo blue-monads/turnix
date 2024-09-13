9ca592b15a8b4e18cf112a185547342f0531b7c5
bornjre <bornjre@gmail.com>
2024-09-13 20:14:20 +0545

    cleanup

--- frontend/src/routes/portal/projects/books/[pid]/sales/new/+page.svelte
+++ frontend/src/routes/portal/projects/books/[pid]/sales/new/+page.svelte
@@ -1,34 +1,14 @@
 <script lang="ts">
-    import { NewBookAPI, type Catagory } from "$lib/projects/books";
+    import { NewBookAPI } from "$lib/projects/books";
     import { getContext } from "svelte";
     import type { RootAPI } from "$lib/api";
     import { page } from "$app/stores";
-    import SvgIcon from "$lib/compo/icons/SvgIcon.svelte";
-    import type { NewSaleLine } from "./sub/sales";
-    import {
-        getModalStore,
-        RadioGroup,
-        RadioItem,
-    } from "@skeletonlabs/skeleton";
     import { Loader } from "$lib/compo";
     import SaleMaker from "./SaleMaker.svelte";
 
     const pid = $page.params["pid"];
     const api = NewBookAPI(getContext("__api__") as RootAPI);
 
-    let message = "";
-
-    const store = getModalStore();
-
-    let title = "";
-    let info = "";
-    let totalPrice = 0;
-    let client_id = 0;
-    let sales_date = new Date().toISOString().slice(0, 16);
-
-    let showTitle = !!title;
-
-    let lines: NewSaleLine[] = [];
     let contacts: object[] = [];
     let loadingContacts = true;
 
@@ -65,7 +45,6 @@
         {api} 
         contactsNameIndex={__clientIndex}
         submit={submit}
-        bind:client_id={client_id}
         
        
         
