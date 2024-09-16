15caa3b3feaaa27f95cab3ec87b4bfde95271235
bornjre <bornjre@gmail.com>
2024-09-17 03:06:29 +0545

    also preview audio

--- frontend/src/routes/portal/self/files/preview/+page.svelte
+++ frontend/src/routes/portal/self/files/preview/+page.svelte
@@ -15,8 +15,19 @@
 
     const imageExts = ["png", "jpg", "jpeg", "gif", "bmp", "webp", "svg"];
     const fileExt = filename.split(".").pop();
-    const isimage = imageExts.includes(fileExt as string);
-    const isVideo = fileExt === "mp4";
+
+    let fileType: "image" | "video" | "audio" | "other" = imageExts.includes(
+        fileExt as string,
+    )
+        ? "image"
+        : fileExt === "mp4"
+        ? "video"
+        : fileExt === "mp3"
+        ? "audio"
+        : "other";
+
+
+
 
     let fileurlWithShortKey = "";
 
@@ -48,17 +59,22 @@
 >
     {#if loading}
         <div>loading...</div>
-    {:else if isimage}
+    {:else if fileType === "image"}
         <img
             class="h-fit w-auto overflow-auto"
             src={fileurlWithShortKey}
             alt={filename}
         />
-    {:else if isVideo}
+    {:else if fileType === "video"}
         <!-- svelte-ignore a11y-media-has-caption -->
         <video controls>
             <source src={fileurlWithShortKey} type="video/mp4" />
         </video>
+    {:else if fileType === "audio"}
+        <!-- svelte-ignore a11y-media-has-caption -->
+        <audio controls>
+            <source src={fileurlWithShortKey} type="audio/mp3" />
+        </audio>
     {:else}
         <button class="btn btn-sm variant-filled" on:click={download}>
             download
