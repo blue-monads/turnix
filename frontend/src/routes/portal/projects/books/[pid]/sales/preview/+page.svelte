2fcc92d79ee783652f3615cb00c423e0e23e4047
bornjre <bornjre@gmail.com>
2024-09-17 02:51:17 +0545

    sales preview ui

--- frontend/src/routes/portal/projects/books/[pid]/sales/preview/+page.svelte
+++ frontend/src/routes/portal/projects/books/[pid]/sales/preview/+page.svelte
@@ -1,6 +1,65 @@
 <script lang="ts">
+    import { NewBookAPI, type Sale, type SalesData } from "$lib/projects/books";
+    import { getContext } from "svelte";
+    import type { RootAPI } from "$lib/api";
+    import { page } from "$app/stores";
+    import { Loader } from "$lib/compo";
+    import { params } from "$lib/params";
+    import SalesPreview from "./SalesPreview.svelte";
 
+    const pid = $page.params["pid"];
+    const sid = $params["sid"];
+
+    const api = NewBookAPI(getContext("__api__") as RootAPI);
+
+    let data: SalesData;
+    let contacts: object[] = [];
+    let loadingContacts = true;
+    let loadingData = true;
+
+    const loadContact = async () => {
+        loadingContacts = true;
+        const resp = await api.listContacts(pid);
+        if (resp.status !== 200) {
+            return;
+        }
+
+        contacts = resp.data;
+        loadingContacts = false;
+    };
+
+    const loadData = async () => {
+        loadingData = true;
+        const resp = await api.getSale(pid, sid);
+        if (resp.status !== 200) {
+            return;
+        }
+        data = resp.data;
+        loadingData = false;
+
+        console.log("@sale_data", resp.data);
+    };
+
+    loadData();
+    loadContact();
+
+    $: __clientIndex = contacts.reduce((acc: any, item: any) => {
+        const item_id = item["id"];
+        const name = item["name"];
+        acc[item_id] = name;
+        return acc;
+    }, {});
+
+    let mode = "invoice";
 </script>
 
-
-<div>preview Sale</div>
+{#if loadingContacts || loadingData}
+    <Loader />
+{:else}
+    <SalesPreview
+        {api}
+        salesData={data}
+        pid={Number(pid)}
+        contactsNameIndex={__clientIndex}
+    />
+{/if}
---
+++ frontend/src/routes/portal/projects/books/[pid]/sales/preview/SalesPreview.svelte
@@ -0,0 +1,263 @@
+<script lang="ts">
+    import SvgIcon from "$lib/compo/icons/SvgIcon.svelte";
+    import type { BooksAPI, Contact, SalesData } from "$lib/projects/books";
+    import {
+        getModalStore,
+        RadioGroup,
+        RadioItem,
+    } from "@skeletonlabs/skeleton";
+    import { formatCurrency } from "$lib";
+
+    const store = getModalStore();
+
+    export let pid: number;
+    export let salesData: SalesData;
+    export let contactsNameIndex: Record<number, string> = {};
+
+    export let api: BooksAPI;
+
+    let overall_tax_percentage = 0;
+    let overall_discount_percentage = 0;
+</script>
+
+<form class="p-2">
+    <div class="card">
+        <header class="card-header flex justify-between">
+            <h3 class="h3">Sales #{salesData.sale.id}</h3>
+        </header>
+
+        <section class="p-4 flex flex-col gap-4">
+            <div class="flex gap-2 justify-between">
+                <div class="flex flex-col gap-2">
+                    <div class="label">
+                        <span class="font-semibold">Title : </span>
+                        <span>{salesData.sale.title || ""}</span>
+                    </div>
+
+                    <div class="label">
+                        <span class="font-semibold">Billed To : </span>
+
+                        <span class="text-sm italic">
+                            {contactsNameIndex[salesData.sale.client_id] || ""}
+                        </span>
+                    </div>
+                </div>
+
+                <label class="label">
+                    <span>Date of Sale</span>
+                    <input
+                        type="datetime-local"
+                        class="input p-1"
+                        placeholder="Date"
+                        disabled
+                        value={new Date(salesData.sale.created_at).toISOString().slice(0, 16)}
+                    />
+                </label>
+            </div>
+
+            <div class="max-h-96 overflow-auto">
+                <table
+                    class="w-full text-left table-auto print:text-sm"
+                    id="table-items"
+                >
+                    <thead>
+                        <tr
+                            class="variant-filled print:bg-gray-300 print:text-black"
+                        >
+                            <th class="px-4 py-2">Item</th>
+                            <th class="px-4 py-2 text-right">Qty</th>
+                            <th class="px-4 py-2 text-right">Unit Price</th>
+                            <th class="px-4 py-2 text-right">Tax</th>
+                            <th class="px-4 py-2 text-right">Total</th>
+                            <th class="px-4 py-2 text-right"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {#each salesData.lines as line, index}
+                            <tr>
+                                <td class="px-4 py-2 border">{line.info}</td>
+                                <td
+                                    class="px-4 py-2 text-right border tabular-nums slashed-zero"
+                                    >{line.qty}</td
+                                >
+
+                                <td
+                                    class="px-4 py-2 text-right border tabular-nums slashed-zero"
+                                >
+
+                                        <span
+                                            >{line.price} - ({line.discount_amount})</span
+                                        >
+                                        =
+                                        <strong>{formatCurrency((line.price || 0 ) - (line.discount_amount || 0) + (line.tax_amount || 0))}</strong>
+                                    
+                                </td>
+                                <td
+                                    class="px-4 py-2 text-right border tabular-nums slashed-zero"
+                                >
+                                    {#if line.tax_amount}
+                                        {line.tax_amount}
+                                    {:else}
+                                        -
+                                    {/if}
+                                </td>
+                                <td
+                                    class="px-4 py-2 text-right border tabular-nums slashed-zero"
+                                >
+                                    (
+                                    <span>
+                                        {line.price}
+                                        {#if line.discount_amount}
+                                            - {line.discount_amount}
+                                        {/if}
+
+                                        {#if line.tax_amount}
+                                            + {line.tax_amount}
+                                        {/if}
+                                    </span>
+
+                                    ) * {line.qty} =
+
+                                    <strong>
+                                        {line.total_amount}
+                                    </strong>
+                                </td>
+                            </tr>
+                        {/each}
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="flex gap-2 w-full justify-between">
+                <label class="label flex flex-col gap-2 w-full md:w-1/2">
+                    <span>Notes</span>
+                    <textarea
+                        class="textarea p-1"
+                        rows="4"
+                        placeholder="Notes about the sale.."
+                    />
+                </label>
+
+                <div class="flex flex-col gap-2 text-sm">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td class="px-2 py-2 border border-gray-400"
+                                    >Total Items TAX</td
+                                >
+                                <td
+                                    class="px-2 py-2 border border-gray-400 text-right"
+                                >
+                                    <span>
+                                        {formatCurrency(
+                                            salesData.sale
+                                                .total_item_tax_amount,
+                                        )}
+                                    </span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="px-2 py-2 border border-gray-400"
+                                    >Total Items Discount</td
+                                >
+                                <td
+                                    class="px-2 py-2 border border-gray-400 text-right"
+                                >
+                                    <span>
+                                        {formatCurrency(
+                                            salesData.sale
+                                                .total_item_discount_amount,
+                                        )}
+                                    </span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td
+                                    class="px-2 py-2 border border-gray-400 border-b-gray-800"
+                                >
+                                    Sub Total
+                                </td>
+                                <td
+                                    class="px-2 py-2 border border-gray-400 border-b-gray-800 text-right"
+                                >
+                                    <strong
+                                        >{formatCurrency(
+                                            salesData.sale.sub_total,
+                                        )}</strong
+                                    >
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="px-2 py-2 border border-gray-800"
+                                    >Overall TAX</td
+                                >
+                                <td
+                                    class="px-2 py-2 border border-gray-800 text-right"
+                                >
+                                    <span>
+                                        <strong>
+                                            {formatCurrency(
+                                                salesData.sale
+                                                    .overall_tax_amount,
+                                            )}
+                                        </strong>
+                                        {#if overall_tax_percentage}
+                                            [{formatCurrency(
+                                                overall_tax_percentage,
+                                            )}%]
+                                        {/if}
+                                    </span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="px-2 py-2 border border-gray-800">
+                                    Overall Discount
+                                </td>
+
+                                <td
+                                    class="px-2 py-2 border border-gray-800 text-right"
+                                >
+                                    <span>
+                                        <strong>
+                                            {formatCurrency(
+                                                salesData.sale
+                                                    .overall_discount_amount,
+                                            )}
+                                        </strong>
+                                        {#if overall_discount_percentage}
+                                            [{formatCurrency(
+                                                overall_discount_percentage,
+                                            )}%]
+                                        {/if}
+                                    </span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td
+                                    class="px-2 py-2 border border-gray-800 font-semibold"
+                                >
+                                    Total
+                                </td>
+                                <td
+                                    class="px-2 py-2 border border-gray-800 text-right font-semibold"
+                                >
+                                    {formatCurrency(salesData.sale.total)}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
+        <footer class="card-footer flex justify-end">
+            <button class="btn variant-soft-error" on:click={() => {}}>
+                delete
+            </button>
+        </footer>
+    </div>
+</form>
