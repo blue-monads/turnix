92e707584ce8623cae401fddd3288401f1aa03b4
bornjre <bornjre@gmail.com>
2024-09-18 11:24:01 +0545

    change sidebar

--- frontend/src/routes/startpage/+layout.svelte
+++ frontend/src/routes/startpage/+layout.svelte
@@ -1,84 +1,87 @@
 <script lang="ts">
-    import {
-        AppBar,
-        AppRail,
-        AppRailAnchor,
-        AppRailTile,
-        AppShell,
-        type DrawerSettings,
-        type PopupSettings,
-    } from "@skeletonlabs/skeleton";
-    import {
-        computePosition,
-        autoUpdate,
-        offset,
-        shift,
-        flip,
-        arrow,
-    } from "@floating-ui/dom";
-    import { getDrawerStore } from "@skeletonlabs/skeleton";
-
-    import { storePopup } from "@skeletonlabs/skeleton";
-
+    import { page } from "$app/stores";
     import SvgIcon from "$lib/compo/icons/SvgIcon.svelte";
+    const pid = $page.params["pid"];
 
-    const drawer = getDrawerStore();
+    let active = true;
 
     let sibarItems: Record<string, any>[] = [
         {
             name: "Home",
             link: "/z/pages/startpage",
             icon: "home",
+            key: undefined,
         },
         {
             name: "Peers",
             link: "/z/pages/startpage/peers",
             icon: "rss",
+            key: "peers",
         },
         {
             name: "Setting",
             link: "/z/pages/startpage/setting",
             icon: "cog",
+            key: "setting",
         },
     ];
 
-    storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+    $: sidebarKey = $page.url.pathname.split("/").at(4);
+    $: console.log(sidebarKey);
 </script>
 
-<AppShell>
+<div
+    class="flex relative h-screen flex-col md:flex-row md:w-full border-r border-gray-800"
+>
     <div
-        slot="sidebarLeft"
-        class="h-screen bg-gradient-to-t variant-gradient-success-warning grad"
+        id="sidebar"
+        class="p-4 flex flex-col gap-2 transition-all duration-300 ease-in-out bg-blend-darken bg-gradient-to-b from-purple-50 shadow
+        {active ? 'w-48' : 'hidden'}"
     >
-        <AppRail width="w-14" background="bg-gradient-to-b from-purple-100 to-indigo-50">
-            <div class="flex flex-col">
-                {#each sibarItems as item, index}
-                    <a
-                        class="p-4 hover:bg-secondary-50 [&>*]:pointer-events-none"
-                        href={item["link"]}
-                    >
-                        <SvgIcon
-                            name={item["icon"] || "link"}
-                            className="w-6 h-6"
-                        />
-                    </a>
+        <div class="p-2 border text-center variant-outline uppercase font-medium font-token">
+            <h4>Startpage</h4>
+        </div>
 
-                    <div
-                        class="card p-2 variant-filled-secondary"
-                        data-popup={`popupHover-${index}`}
-                    >
-                        <p>{item["name"] || ""}</p>
-                    </div>
-                {/each}
-            </div>
-
-            <div slot="trail" class="mb-4 p-2">
-                <span></span>
-            </div>
-        </AppRail>
+        {#each sibarItems as item}
+            <a
+                href={item.link}
+                class="flex items-center px-2 py-2 w-full text-gray-600 transition-colors duration-300 transform rounded-lg hover:bg-gray-300 hover:text-gray-700 {sidebarKey ===
+                item.key
+                    ? 'bg-gray-400 text-gray-900'
+                    : ''}"
+            >
+                <SvgIcon className="h-4 w-4 mr-1" name={item.icon} />
+                {item.name}
+            </a>
+        {/each}
     </div>
 
-    <div class="h-[calc(100vh-4rem)] md:h-screen bg-gradient-to-b from-indigo-50 to-purple-50">
+    <div class="absolute md:bottom-8">
+        <button
+            class=" rounded border-2 bg-slate-100 p-1"
+            on:click={() => {
+                active = !active;
+            }}
+        >
+            <span class="hidden md:block">
+                <SvgIcon
+                    className="h-4 w-4"
+                    name={active
+                        ? "chevron-double-left"
+                        : "chevron-double-right"}
+                />
+            </span>
+
+            <span class="block md:hidden">
+                <SvgIcon
+                    className="h-4 w-4"
+                    name={active ? "chevron-double-up" : "chevron-double-down"}
+                />
+            </span>
+        </button>
+    </div>
+
+    <main class="grow w-full h-screen overflow-auto">
         <slot />
-    </div>
-</AppShell>
+    </main>
+</div>
