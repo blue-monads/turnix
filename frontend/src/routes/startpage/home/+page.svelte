7e31b09b0113ab413c2f8c570b3ea90b4b6f4329
bornjre <bornjre@gmail.com>
2024-09-21 08:26:39 +0545

    nit

--- frontend/src/routes/startpage/home/+page.svelte
+++ frontend/src/routes/startpage/home/+page.svelte
@@ -1,35 +1,54 @@
-
 <script lang="ts">
     import { AppBar } from "@skeletonlabs/skeleton";
 
+    export let isHomePage = true;
+
     let isInstanceRunning = false;
     let workingDir = "/home/xyz/Desktop/mnop";
-    
 </script>
 
 <div class="flex items-center justify-center min-h-screen flex-col gap-4 pt-20">
     {#if isInstanceRunning}
         <h2 class="h2">Turnix is running.</h2>
         <span class="text-indigo-500 chip">{workingDir} </span>
-        <p class="p text-xl">click explore to start using it. Or click on options.</p>
+        <p class="p text-xl">
+            click explore to start using it. Or click on
+            {#if isHomePage}
+                <span>options</span>
+            {:else}
+                <span>sidebar</span>
+            {/if}
+        </p>
 
         <div class="flex gap-2">
-            <button class="btn variant-soft-primary">Explore</button>   
-            <button class="btn variant-soft-secondary">Options</button>
-        </div>        
-
+            <button class="btn variant-soft-primary">Explore</button>
+            {#if isHomePage}
+                <a href="/z/pages/startpage/home" class="btn variant-soft-secondary"
+                    >Options</a
+                >
+            {/if}
+        </div>
     {:else}
         <h2 class="h2">Turnix is not running</h2>
         <p class="p text-xl">
             Click start to run a instance in current directory.
         </p>
-        <p class="p text-xl">Or click on options</p>
+        <p class="p text-xl">
+            Or click on
+            {#if isHomePage}
+                <span>options</span>
+            {:else}
+                <span>sidebar</span>
+            {/if}
+        </p>
 
         <div class="flex gap-2">
             <button class="btn variant-filled">Start</button>
-            <button class="btn variant-soft-secondary">Options</button>
-        </div>        
-
-
+            {#if isHomePage}
+                <a href="/z/pages/startpage/home" class="btn variant-soft-secondary"
+                    >Options</a
+                >
+            {/if}
+        </div>
     {/if}
 </div>
