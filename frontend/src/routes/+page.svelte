2faf15bcf49c2c80a683f8667d916566671d3200
bornjre <bornjre@gmail.com>
2024-10-06 14:03:51 +0545

    fix pages page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -10,13 +10,30 @@
 <div class="w-full h-full">
 	<AppBar>
 		<svelte:fragment slot="lead">Turnix</svelte:fragment>
-		
-		<svelte:fragment slot="trail">
-			<a class="hover:underline" href="/z/pages/portal">Portal</a>
-			<a class="hover:underline" href="/z/pages/auth/login">Login</a>
-			<a class="hover:underline" href="/z/pages/auth/login">Signup</a>
-		</svelte:fragment>
-
-
 	</AppBar>
+
+
+	<div class="my-10 mx-auto w-96 p-4">
+	
+		<div class="flex flex-col gap-2 card p-4">
+
+			<h3 class="h4">Pages</h3>
+
+			<div class="flex flex-col pl-2">
+
+				<ul class="list-disc list-inside">
+					<li><a class="hover:underline" href="/z/pages/portal">Portal</a></li>
+					<li><a class="hover:underline" href="/z/pages/auth/login">Login</a></li>
+					<li><a class="hover:underline" href="/z/pages/auth/login">Signup</a></li>
+				</ul>
+
+			</div>
+		</div>
+	
+	</div>
+
+	
+
+
+
 </div>
