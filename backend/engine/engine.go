3d15ad67600e0db58e389a63a76a113a1f6189f4
bornjre <bornjre@gmail.com>
2024-11-09 14:43:22 +0545

    nit

--- backend/engine/engine.go
+++ backend/engine/engine.go
@@ -1,25 +1,16 @@
 package engine
 
 import (
-	"sync"
-
 	pluginengine "github.com/bornjre/turnix/backend/engine/pluginAction"
 	"github.com/bornjre/turnix/backend/engine/pool"
 
 	"github.com/bornjre/turnix/backend/services/database"
 	"github.com/bornjre/turnix/backend/services/signer"
-	"github.com/bornjre/turnix/backend/xtypes/xengine"
 	"github.com/rs/zerolog"
 )
 
 type Engine struct {
-	hLock        sync.RWMutex
 	pluginEngine *pluginengine.PluginAction
-}
-
-type handlerRef struct {
-	handler  xengine.EventHandler
-	priority int16
 }
 
 func New(db *database.DB, signer *signer.Signer, logger zerolog.Logger) *Engine {
@@ -27,7 +18,6 @@ func New(db *database.DB, signer *signer.Signer, logger zerolog.Logger) *Engine 
 	gpool := pool.New(logger)
 
 	return &Engine{
-		hLock:        sync.RWMutex{},
 		pluginEngine: pluginengine.New(gpool),
 	}
 
