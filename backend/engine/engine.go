e0ac75f25c9d22f82f92b695446695aacc5ceec8
bornjre <bornjre@gmail.com>
2024-11-09 14:42:39 +0545

    remove old hook exec system

--- backend/engine/engine.go
+++ backend/engine/engine.go
@@ -3,21 +3,17 @@ package engine
 import (
 	"sync"
 
-	hookengine "github.com/bornjre/turnix/backend/engine/hookEngine"
 	pluginengine "github.com/bornjre/turnix/backend/engine/pluginAction"
 	"github.com/bornjre/turnix/backend/engine/pool"
 
 	"github.com/bornjre/turnix/backend/services/database"
 	"github.com/bornjre/turnix/backend/services/signer"
 	"github.com/bornjre/turnix/backend/xtypes/xengine"
-	"github.com/rs/xid"
 	"github.com/rs/zerolog"
 )
 
 type Engine struct {
-	handlers     map[string][]handlerRef
 	hLock        sync.RWMutex
-	hookEngine   *hookengine.HookEngine
 	pluginEngine *pluginengine.PluginAction
 }
 
@@ -31,75 +27,19 @@ func New(db *database.DB, signer *signer.Signer, logger zerolog.Logger) *Engine 
 	gpool := pool.New(logger)
 
 	return &Engine{
-		handlers: make(map[string][]handlerRef),
-		hLock:    sync.RWMutex{},
-		hookEngine: hookengine.New(hookengine.Options{
-			DB:       db,
-			Signer:   signer,
-			Logger:   logger,
-			GojaPool: gpool,
-		}),
+		hLock:        sync.RWMutex{},
 		pluginEngine: pluginengine.New(gpool),
 	}
 
 }
 
 func (e *Engine) Init() error {
-	return e.hookEngine.Init()
+
+	return nil
 }
 
 func (e *Engine) Invalidate(pid int64) error {
-	return e.hookEngine.Invalidate(pid)
-}
-
-func (e *Engine) Emit(ev xengine.EventNew) (*xengine.EventResult, error) {
-
-	e.hLock.RLock()
-
-	handlers := e.handlers[ev.Type]
-
-	e.hLock.RUnlock()
-
-	ctx := xengine.EventContext{
-		EventId:       xid.New().String(),
-		Event:         &ev,
-		PreventAction: false,
-		Data:          map[string]any{},
-	}
-
-	for _, hr := range handlers {
-		err := hr.handler(ctx)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	if ev.Project == 0 || !ev.AllowHook {
-		return &xengine.EventResult{
-			PreventAction: ctx.PreventAction,
-			Errors:        map[string]string{},
-			Data:          ctx.Data,
-		}, nil
-	}
-
-	return e.hookEngine.Emit(ctx)
-
-}
-
-func (e *Engine) OnEvent(name string, handler xengine.EventHandler, priority int16) {
-
-	e.hLock.Lock()
-	defer e.hLock.Unlock()
-
-	handlers := e.handlers[name]
-
-	handlers = append(handlers, handlerRef{
-		handler:  handler,
-		priority: priority,
-	})
-
-	e.handlers[name] = handlers
-
+	return nil
 }
 
 func (e *Engine) Stop(force bool) error {
