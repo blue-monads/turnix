272ba5ae38354f975dcfd49dde13325b1d2a8e5e
bornjre <bornjre@gmail.com>
2024-11-10 15:21:52 +0545

    move pluginAction code to engine

--- backend/engine/engine.go
+++ backend/engine/engine.go
@@ -1,37 +1,125 @@
 package engine
 
 import (
-	pluginengine "github.com/bornjre/turnix/backend/engine/pluginAction"
 	"github.com/bornjre/turnix/backend/engine/pool"
 
+	"encoding/json"
+	"fmt"
+
 	"github.com/bornjre/turnix/backend/services/database"
-	"github.com/bornjre/turnix/backend/services/signer"
-	"github.com/rs/zerolog"
+	"github.com/dop251/goja"
 )
 
 type Engine struct {
-	pluginEngine *pluginengine.PluginAction
+	pool *pool.GojaPool
+	db   *database.DB
 }
 
-func New(db *database.DB, signer *signer.Signer, logger zerolog.Logger) *Engine {
-
-	gpool := pool.New(logger)
-
+func New(pool *pool.GojaPool) *Engine {
 	return &Engine{
-		pluginEngine: pluginengine.New(gpool),
+		pool: pool,
+	}
+}
+
+func (p *Engine) Run(pid, plugId int64, name string, data map[string]any) (any, error) {
+
+	jsHandle := p.pool.Get(pid, true)
+
+	if jsHandle == nil {
+		return nil, fmt.Errorf("no goja handle found")
 	}
 
+	defer p.pool.Set(jsHandle)
+
+	if jsHandle.LastPid != pid {
+
+		plugin, err := p.db.GetProjectPlugin(0, pid, plugId)
+		if err != nil {
+			return nil, err
+		}
+
+		pgm, err := goja.Compile(fmt.Sprintf("project_plugin_%d", pid), plugin.ServerCode, true)
+		if err != nil {
+			return nil, err
+		}
+
+		jsHandle.LastPid = 0
+		_, err = jsHandle.JS.RunProgram(pgm)
+		if err != nil {
+			return nil, err
+		}
+
+		jsHandle.Bind()
+		jsHandle.JS.SetFieldNameMapper(goja.TagFieldNameMapper("json", true))
+
+		jsHandle.LastPid = pid
+	}
+
+	var entry func(ctx *goja.Object)
+
+	eval := jsHandle.JS.Get(name)
+	if eval == nil {
+		return nil, fmt.Errorf("%s function not found in script", name)
+	}
+
+	err := jsHandle.JS.ExportTo(eval, &entry)
+	if err != nil {
+		return nil, err
+	}
+
+	obj := buildEventObject(jsHandle.JS, data)
+
+	entry(obj)
+
+	return nil, nil
 }
 
-func (e *Engine) Init() error {
+func (p *Engine) LiveRun(pid int64, name, source string, data map[string]any) (any, error) {
 
-	return nil
+	rt := goja.New()
+
+	_, err := rt.RunString(source)
+	if err != nil {
+		return nil, err
+	}
+
+	var entry func(ctx *goja.Object)
+
+	eval := rt.Get(name)
+	if eval == nil {
+		return nil, fmt.Errorf("%s function not found in script", name)
+	}
+
+	err = rt.ExportTo(eval, &entry)
+	if err != nil {
+		return nil, err
+	}
+
+	obj := buildEventObject(rt, data)
+
+	entry(obj)
+
+	return nil, nil
+
 }
 
-func (e *Engine) Invalidate(pid int64) error {
-	return nil
-}
+func buildEventObject(jsEngine *goja.Runtime, data map[string]any) *goja.Object {
+	obj := jsEngine.NewObject()
 
-func (e *Engine) Stop(force bool) error {
-	return nil
+	obj.Set("dataAsObject", func() any {
+
+		return data
+	})
+
+	obj.Set("dataAsJsonString", func() any {
+
+		out, err := json.Marshal(data)
+		if err != nil {
+			return nil
+		}
+
+		return string(out)
+	})
+
+	return obj
 }
