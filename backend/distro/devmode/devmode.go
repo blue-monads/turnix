a7eaff6b791801f7722c3da66964795fb5c8aea1
bornjre <bornjre@gmail.com>
2024-11-23 20:42:35 +0545

    move dev cli so its imporatble from external package

---
+++ backend/distro/devmode/devmode.go
@@ -0,0 +1,37 @@
+package devmode
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+
+	// modules
+
+	"github.com/bornjre/turnix/backend/distro"
+)
+
+func Run() {
+
+	app, err := distro.NewApp()
+	if err != nil {
+		panic(err)
+	}
+
+	defer app.Stop()
+
+	mig, err := app.NeedsMigrate()
+	if err != nil {
+		panic(err)
+	}
+
+	if mig {
+		err = app.RunNormalSeed()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = app.Start()
+	if err != nil {
+		panic(err)
+	}
+
+}
