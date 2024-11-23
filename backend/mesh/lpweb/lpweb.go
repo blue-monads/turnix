d18c228a96f4df02ee8e69f505049ff5a8fe1a0e
bornjre <bornjre@gmail.com>
2024-11-24 01:51:29 +0545

    nit

--- backend/mesh/lpweb/lpweb.go
+++ backend/mesh/lpweb/lpweb.go
@@ -13,14 +13,15 @@ type LPWebMesh struct {
 	proxy  *proxy.WebProxy
 }
 
-func New(key string, tunnelPort, proxyPort int) *LPWebMesh {
+func New(key string, meshPort, tunnelPort int) *LPWebMesh {
 
-	m, err := mesh.New(key, 0)
+	m, err := mesh.New(key, meshPort)
 	if err != nil {
 		panic(err)
 	}
 
-	wproxy := proxy.NewUsingMesh(proxyPort, m)
+	// we are not going to do `wproxy.Run()` so port doesnot matter
+	wproxy := proxy.NewUsingMesh(0, m)
 	tunnel := tunnel.NewUsingMesh(tunnelPort, m, false)
 
 	return &LPWebMesh{
