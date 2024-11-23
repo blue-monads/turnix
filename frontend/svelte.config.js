f89af87797b5d0cda52fe6794f90ce66d11851bd
bornjre <bornjre@gmail.com>
2024-11-23 20:58:26 +0545

    make build deterministic

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const isDev = process.env.NODE_ENV === "development";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [vitePreprocess({})],
@@ -16,9 +18,9 @@ const config = {
     paths: {
       base: "/z/pages"
     },
-    // version: {
-    //   name: "0.1.12",
-    // }
+    version: isDev ? undefined : {
+      name: "0.1.12",
+    }
   },
 };
 
