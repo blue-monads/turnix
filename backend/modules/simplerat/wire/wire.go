7fa067e9476f73ad1254661c07d09203cb31817b
bornjre <bornjre@gmail.com>
2025-01-28 14:50:22 +0545

    new binary protocal

--- backend/modules/simplerat/wire/wire.go
+++ backend/modules/simplerat/wire/wire.go
@@ -2,14 +2,21 @@ package wire
 
 import "encoding/binary"
 
+const (
+	PtypeRequest  = 0
+	PtypeResponse = 1
+	PtypePartial  = 2
+)
+
 type WSPacket struct {
 	Ptype uint8
 	Mid   uint64
+	Pid   uint16
 	MType string
 }
 
 func (p *WSPacket) Size() int {
-	return 1 + 8 + 1 + len(p.MType)
+	return 1 + 8 + 2 + 1 + len(p.MType)
 }
 
 func Pack(p *WSPacket) []byte {
@@ -24,8 +31,10 @@ func Pack(p *WSPacket) []byte {
 
 	buf[0] = p.Ptype
 	binary.BigEndian.PutUint64(buf[1:], p.Mid)
-	buf[9] = byte(mtypeLen)
-	copy(buf[10:], p.MType)
+	binary.BigEndian.PutUint16(buf[9:], p.Pid)
+
+	buf[11] = uint8(mtypeLen)
+	copy(buf[12:], []byte(p.MType))
 
 	return buf
 }
@@ -36,9 +45,10 @@ func Unpack(buf []byte) *WSPacket {
 	p := &WSPacket{}
 	p.Ptype = buf[0]
 	p.Mid = binary.BigEndian.Uint64(buf[1:9])
+	p.Pid = binary.BigEndian.Uint16(buf[9:11])
 
-	mtypeLen := buf[9]
-	p.MType = string(buf[10 : 10+mtypeLen])
+	mtypeLen := int(buf[11])
+	p.MType = string(buf[12 : 12+mtypeLen])
 
 	return p
 }
--- cmd/play/play.go
+++ cmd/play/play.go
@@ -1,7 +1,40 @@
 package main
 
-import "github.com/k0kubun/pp"
+import (
+	"github.com/blue-monads/turnix/backend/modules/simplerat/wire"
+	"github.com/k0kubun/pp"
+)
 
 func main() {
 	pp.Println("@play")
+
+	p := &wire.WSPacket{
+		Ptype: wire.PtypeRequest,
+		Mid:   1,
+		Pid:   2,
+		MType: "hello",
+	}
+
+	out := wire.Pack(p)
+
+	pp.Println(out)
+
+	up := wire.Unpack(out)
+
+	if p.Ptype != up.Ptype {
+		panic("Ptype mismatch")
+	}
+
+	if p.Mid != up.Mid {
+		panic("Mid mismatch")
+	}
+
+	if p.Pid != up.Pid {
+		panic("Pid mismatch")
+	}
+
+	if p.MType != up.MType {
+		panic("MType mismatch")
+	}
+
 }
