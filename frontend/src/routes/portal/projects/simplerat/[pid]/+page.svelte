22a4ef1e0b6d806f93898eeb666ac8062bfd7dd8
bornjre <bornjre@gmail.com>
2025-01-28 18:02:33 +0545

    cleanup

--- frontend/src/routes/portal/projects/simplerat/[pid]/+page.svelte
+++ frontend/src/routes/portal/projects/simplerat/[pid]/+page.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
     import { AutoTable } from "$lib/compo";
     import { getContext } from "svelte";
-    import { AppBar, getModalStore } from "@skeletonlabs/skeleton";
+    import { AppBar } from "@skeletonlabs/skeleton";
     import type { RootAPI } from "$lib/api";
     import { page } from "$app/stores";
     import { NewSimpleRATApi } from "../lib/SimpleRATApi";
     import SvgIcon from "$lib/compo/icons/SvgIcon.svelte";
     import AddDeviceDialog from "./AddDeviceDialog.svelte";
     import { goto } from "$app/navigation";
-    import { webspcketTest } from "./wstest";
     import SystemInfoDialog from "./SystemInfoDialog.svelte";
     import ScreenshotDialog from "./ScreenshotDialog.svelte";
 
     const pid = $page.params["pid"];
 
-    const store = getModalStore();
     const api = NewSimpleRATApi(getContext("__api__") as RootAPI);
     const modal = getContext("__vs_modal__") as any;
 
