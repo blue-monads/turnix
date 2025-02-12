4df6021ab4a9059af9f55b74ed6ab9b3512e24c9
bornjre <bornjre@gmail.com>
2025-02-13 04:06:48 +0545

    shared project listing page

---
+++ frontend/src/routes/portal/project/list/[ptype]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import ListProject from "$lib/container/project/listProject.svelte";
+    const ptype = $page.params["ptype"];
+</script>
+
+<ListProject ptype={ptype} />
+  
