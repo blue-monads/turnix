2b8fa63cf0fc9482b4c88b14f579843bda1e1de8
bornjre <bornjre@gmail.com>
2025-02-14 01:40:59 +0545

    nit

--- cmd/dev/main.go
+++ cmd/dev/main.go
@@ -1,38 +1,35 @@
 package main
 
 import (
-	"embed"
-
 	_ "github.com/mattn/go-sqlite3"
 
 	// modules
 
 	"github.com/blue-monads/turnix/backend/distro/devmode"
-	"github.com/blue-monads/turnix/backend/registry"
-	"github.com/blue-monads/turnix/backend/xtypes/xproject"
 
 	_ "github.com/blue-monads/turnix/backend/modules/books"
+	_ "github.com/blue-monads/turnix/backend/modules/zblog"
 
 	// _ "github.com/blue-monads/turnix/backend/modules/tracker"
 	_ "github.com/blue-monads/turnix/backend/modules/unloop"
 )
 
-//go:embed all:assets/*
-var devModeAssets embed.FS
+// //go:embed all:assets/*
+// var devModeAssets embed.FS
 
 func main() {
 
-	registry.Register("devmode", func(opt xproject.BuilderOption) (*xproject.Defination, error) {
-		return &xproject.Defination{
-			Name:            "Devmode",
-			Slug:            "devmode",
-			Info:            "Development mode",
-			Version:         "0.0.1",
-			LinkPattern:     "/z/x/devmode",
-			AssetData:       devModeAssets,
-			AssetDataPrefix: "assets",
-		}, nil
-	})
+	// registry.Register("devmode", func(opt xproject.BuilderOption) (*xproject.Defination, error) {
+	// 	return &xproject.Defination{
+	// 		Name:            "Devmode",
+	// 		Slug:            "devmode",
+	// 		Info:            "Development mode",
+	// 		Version:         "0.0.1",
+	// 		LinkPattern:     "/z/x/devmode",
+	// 		AssetData:       devModeAssets,
+	// 		AssetDataPrefix: "assets",
+	// 	}, nil
+	// })
 
 	devmode.Run()
 
