6062e14d9223073d4d2dd2632ce5bd732f96dc7e
bornjre <bornjre@gmail.com>
2025-03-16 20:44:00 +0545

    nit

--- cmd/luaplay/main.go
+++ cmd/luaplay/main.go
@@ -19,20 +19,6 @@ func main() {
 	L.SetField(tb, "name", lua.LString("john"))
 	L.SetGlobal("tb", tb)
 
-	// Create handle module table
-	handleT := L.NewTable()
-	L.SetFuncs(handleT, map[string]lua.LGFunction{
-		"hello": func(L *lua.LState) int {
-			L.Push(lua.LString("hello from go"))
-			return 1
-		},
-		"return_two_things": func(L *lua.LState) int {
-			L.Push(lua.LString("jamba"))
-			L.Push(lua.LString("juce"))
-			return 2
-		},
-	})
-
 	// Register the module loader
 	L.PreloadModule("handle", func(L *lua.LState) int {
 		// Create a copy of the handleT table to return
