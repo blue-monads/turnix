e485ddaff5667b8e607e4f4a3209d3abaa8cc743
bornjre <bornjre@gmail.com>
2025-03-29 04:55:49 +0545

    shared lib for papps wip

--- frontend/output/appcore.js
+++ frontend/output/appcore.js
@@ -1,15 +1,53 @@
-// attach to the window object
 window.appcore = {
+    _onInit: [],
+
+    onInit: (callback) => {
+        appcore._onInit.push(callback);
+    },
+
     isLoggedIn: () => {
-        return false;
+        return !!localStorage.getItem("access_token");
     },
 
-    getUserInfo: () => {
-        return null;
+    mustGetAccessToken: () => {
+        const accessToken = localStorage.getItem("access_token");
+
+        if (!accessToken) {
+            appcore.redirectToLogin();
+            return null;
+        }
+
+        return accessToken;
     },
 
-    getBaseUrl: () => {
+    ensureLoggedIn: () => {
+        if (!appcore.isLoggedIn()) {
+            appcore.redirectToLogin();
+            return false;
+        }
+        return true;
+    },
 
+    getHttpClient: () => {
+        const accessToken = appcore.mustGetAccessToken();
+
+        if (typeof axios === "undefined") {
+            throw new Error("axios is not defined");
+        }
+
+        return axios.create({
+            baseURL: appcore.getBaseURL(),
+            headers: {
+                "Authorization": accessToken
+            }
+        });
+    },    
+    getBaseURL: () => {
+        return `${location.origin}/z/project/`;
+    },
+
+    getProjectURL: (ptype) => {
+        return `${location.origin}/z/project/${ptype}/`;
     },
 
     redirectToLogin: () => {
@@ -19,6 +57,17 @@ window.appcore = {
     redirectToHome: () => {
         window.location.href = "/";
     },
+
+    redirectToPortal: () => {
+        window.location.href = `${location.origin}/z/pages/portal`;
+    },
 }
 
 
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOMContentLoaded/appcore");
+
+    for (const callback of appcore._onInit) {
+        callback();
+    }
+});
