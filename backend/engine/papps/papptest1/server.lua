cc801f4e39f588839113db6c5066f0c7a0756fcc
bornjre <bornjre@gmail.com>
2025-03-29 13:42:48 +0545

    test app

---
+++ backend/engine/papps/papptest1/server.lua
@@ -0,0 +1,15 @@
+function handle(ctx)
+    print("PApp Test 1")
+end
+
+function testHandler1(ctx)
+    print("PApp Test 1")
+end
+
+function testHandler2(ctx)
+    print("PApp Test 1")
+end
+
+function testHandler3(ctx)
+    print("PApp Test 1")
+end
