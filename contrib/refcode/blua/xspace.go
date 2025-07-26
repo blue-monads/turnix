e02d14f42b8ced38797f1e83f73666ac89d5e22b
bornjre <bornjre@gmail.com>
2025-07-26 10:56:38 +0545

    nit

--- contrib/refcode/blua/xspace.go
+++ contrib/refcode/blua/xspace.go
@@ -8,29 +8,6 @@ import (
 	"github.com/rs/zerolog"
 )
 
-/*
-
-ROUTES:
- /z/startup/
- /z/preload/spaces/<space_type>/
- /z/pages/startup/
- /z/pages/portal/spaces/<space_type>/
- /z/spaces/<space_type>/
- /z/api/spaces/<space_type>/
-TABLE:
- zSpaceXXX
- zspace_pid_xxx
- zBuddyXXX
- zbuddy_bid_xxx
- zCaptureXXX
- zcapture_chash_xxx
- zLogXXX
-WORKING_DIR:
- /workin_space/<space_type>
-
-
-*/
-
 type BuilderOption struct {
 	App xtypes.App
 	DB  *database.DB
